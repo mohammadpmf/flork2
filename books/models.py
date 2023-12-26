7492ecd01ab549fbf4aa65a4a4e7820c5d2ced99
mohammadpmf <mohammad.pfallah@gmail.com>
2023-12-26 16:15:34 +0330

    5 Dey Book Model Added

--- books/models.py
+++ books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length = 60)
+    author = models.CharField(max_length = 100)
+    translator = models.CharField(max_length=100, blank=True)
+    publisher = models.CharField(max_length=100, blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.TextField()
+    cover = models.ImageField(upload_to='covers/', blank=True)
+    
+    def __str__(self):
+        return f"{self.author}: {self.title}"
+
--- config/settings.py
+++ config/settings.py
@@ -129,3 +129,6 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
