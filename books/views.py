268c5f583a86df54902fd080657812dbe04b4e51
mohammadpmf <mohammad.pfallah@gmail.com>
2024-01-02 16:16:49 +0330

    book.cover.url added

--- books/views.py
+++ books/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Book
 
-# Create your views here.
+class BooksList(generic.ListView):
+    model = Book
+    context_object_name = 'books'
+    template_name = 'book_list.html'
+    
