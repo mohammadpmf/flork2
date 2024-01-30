31c04d9d67a2e4d8564a0ce8a9087ea33fafe9f1
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-01-30 17:40:15 +0330

    After merge 10 Bahman

--- books/urls.py
+++ books/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.BooksList.as_view(), name='book_list'),
     path('new_book/', views.AddBook.as_view(), name='new_book'),
+    path('delete/<int:pk>/', views.DeleteBook.as_view(), name='delete'),
 ]
--- books/views.py
+++ books/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 from .models import Book
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class BooksList(generic.ListView):
     model = Book
@@ -12,5 +13,11 @@ class BooksList(generic.ListView):
 class AddBook(generic.CreateView):
     model = Book
     template_name = 'book_new.html'
-    fields = '__all__'
+    fields = ['title','author','translator','publisher','price','description','cover']
+    # fields = '__all__'
+    success_url = reverse_lazy('book_list')
+
+class DeleteBook(LoginRequiredMixin, generic.DeleteView):
+    model = Book
+    template_name = 'book_delete.html'
     success_url = reverse_lazy('book_list')
